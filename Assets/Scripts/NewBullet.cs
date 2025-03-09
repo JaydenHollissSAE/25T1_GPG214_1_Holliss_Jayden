1dcd2be81c4d2edfd5018fa357eed49c25a41303
JaydenHollissSAE <159383304+JaydenHollissSAE@users.noreply.github.com>
2025-03-09 22:18:59 +1100

    Restored individual streaming as option and fixed no existant errors

--- Assets/Scripts/NewBullet.cs
+++ Assets/Scripts/NewBullet.cs
@@ -61,101 +61,122 @@ public class NewBullet : MonoBehaviour
         if (canRun)
         {
 
-
+            bool changesFound = false;
             if (bullets.Count > 0)
             {
-
-                // Old individual loading system
-
-                //string directory = Path.Combine(Application.streamingAssetsPath, newBulletType);
-                //Sprite newSprite = null;
-                //AudioClip audioClip = null;
-                //if (Directory.Exists(directory))
-                //{
-                //    if (File.Exists(Path.Combine(directory, newBulletTextureName)))
-                //    {
-                //        string spritePath = Path.Combine(directory, newBulletTextureName);
-                //        byte[] spriteBytes = File.ReadAllBytes(spritePath);
-                //        Texture2D texture = new Texture2D(2, 2);
-                //        texture.LoadImage(spriteBytes);
-                //        newSprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0), texture.height);
-
-                //        //spriteRenderer.sprite = newSprite;
-                //    }
-                //    else
-                //    {
-                //        Debug.LogError(newBulletTextureName + " file not found in [StreamingAssets/" + newBulletType + "] folder. Please make sure the file exists and is named correctly.");
-                //    }
-
-                //    if (File.Exists(Path.Combine(directory, newBulletAudioName)))
-                //    {
-                //        string audioPath = Path.Combine(directory, newBulletAudioName);
-                //        byte[] audioData = File.ReadAllBytes(audioPath);
-                //        float[] floatArray = new float[audioData.Length / 2];
-                //        for (int i = 0; i < floatArray.Length; i++)
-                //        {
-                //            short bitValue = System.BitConverter.ToInt16(audioData, i * 2);
-                //            floatArray[i] = bitValue / 32768f;
-                //        }
-
-                //        audioClip = AudioClip.Create("AudioClip", floatArray.Length, 1, 44100, false);
-                //        audioClip.SetData(floatArray, 0);
-                //    }
-                //    else
-                //    {
-                //        Debug.LogError(newBulletAudioName + " file not found in [StreamingAssets/"+ newBulletType+"] folder. Please make sure the file exists and is named correctly.");
-                //    }
+                Sprite newSprite = null;
+                AudioClip audioClip = null;
 
 
-                //}
-                //else
-                //{
-                //    Debug.LogError(newBulletType + " folder not found in [StreamingAssets] folder");
-                //}
-
-                
                 // New Asset Bundle Loading system
 
                 string path = Path.Combine(Application.streamingAssetsPath, "Bullets", newBulletType);
 
-
-                AssetBundle newBullet = null;
-                if (File.Exists(path))
+                if (File.Exists(path) && File.Exists(path+ ".manifest"))
                 {
-                    newBullet = AssetBundle.LoadFromFile(path);
+
+
+                    AssetBundle newBullet = null;
+                    if (File.Exists(path))
+                    {
+                        newBullet = AssetBundle.LoadFromFile(path);
+                    }
+                    else
+                    {
+                        Debug.LogError("No Asset Bundle of the name [" + newBulletType + "] exists in [StreamingAssets/Bullets]");
+                    }
+
+                    if (newBullet != null)
+                    {
+                        Texture2D spriteTexture = newBullet.LoadAsset<Texture2D>(newBulletTextureName);
+                        newSprite = Sprite.Create(spriteTexture, new Rect(0, 0, spriteTexture.width, spriteTexture.height), new Vector2(0.5f, 0), spriteTexture.height);
+
+                        audioClip = newBullet.LoadAsset<AudioClip>(newBulletAudioName);
+                        changesFound = true;
+
+                    }
+                    else
+                    {
+                        Debug.LogError("The [" + newBulletType + "] Asset Bundle within the in the [StreamingAssets/Bullets] contains no data.");
+                    }
+
+                }
+
+                else if (Directory.Exists(path))
+                {
+
+                    // Old individual loading system
+
+                    string directory = path;
+                    if (Directory.Exists(directory))
+                    {
+                        if (File.Exists(Path.Combine(directory, newBulletTextureName)))
+                        {
+                            string spritePath = Path.Combine(directory, newBulletTextureName);
+                            byte[] spriteBytes = File.ReadAllBytes(spritePath);
+                            Texture2D texture = new Texture2D(2, 2);
+                            texture.LoadImage(spriteBytes);
+                            newSprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0), texture.height);
+                            changesFound = true;
+
+                            //spriteRenderer.sprite = newSprite;
+                        }
+                        else
+                        {
+                            Debug.LogError("[" + newBulletTextureName + "] file not found in [StreamingAssets/Bullets" + newBulletType + "] folder. Please make sure the file exists and is named correctly.");
+                        }
+
+                        if (File.Exists(Path.Combine(directory, newBulletAudioName)))
+                        {
+                            string audioPath = Path.Combine(directory, newBulletAudioName);
+                            byte[] audioData = File.ReadAllBytes(audioPath);
+                            float[] floatArray = new float[audioData.Length / 2];
+                            for (int i = 0; i < floatArray.Length; i++)
+                            {
+                                short bitValue = System.BitConverter.ToInt16(audioData, i * 2);
+                                floatArray[i] = bitValue / 32768f;
+                            }
+
+                            audioClip = AudioClip.Create("AudioClip", floatArray.Length, 1, 44100, false);
+                            audioClip.SetData(floatArray, 0);
+                            changesFound = true;
+                        }
+                        else
+                        {
+                            Debug.LogError("["+newBulletAudioName + "] file not found in [StreamingAssets/Bullets/" + newBulletType + "] folder. Please make sure the file exists and is named correctly.");
+                        }
+
+
+                    }
+                    else
+                    {
+                        Debug.LogError("["+newBulletType + "] folder not found in [StreamingAssets/Bullets] folder");
+                    }
                 }
                 else
                 {
-                    Debug.LogError("No Asset Bundle of the name "+newBulletType+" exists in"+ Path.Combine(Application.streamingAssetsPath, "Bullets"));
-                }
-                Sprite newSprite = null;
-                AudioClip audioClip = null;
-
-                if (newBullet != null)
-                {
-                    Texture2D spriteTexture = newBullet.LoadAsset<Texture2D>(newBulletTextureName);
-                    newSprite = Sprite.Create(spriteTexture, new Rect(0, 0, spriteTexture.width, spriteTexture.height), new Vector2(0.5f, 0), spriteTexture.height);
-
-                    audioClip = newBullet.LoadAsset<AudioClip>(newBulletAudioName);
-
+                    Debug.LogError("No item called ["+newBulletType + "] exists in the [StreamingAssets/Bullets] folder");
                 }
 
-
-                foreach (Bullet bullet in bullets)
+                if (changesFound)
                 {
-                    GameObject bulletObject = bullet.gameObject;
-                    if (!bulletObject.active)
+                    foreach (Bullet bullet in bullets)
                     {
-                        if (newSprite != null)
+                        GameObject bulletObject = bullet.gameObject;
+                        if (!bulletObject.active)
                         {
-                            bulletObject.GetComponent<SpriteRenderer>().sprite = newSprite;
-                        }
-                        if (audioClip != null)
-                        {
-                            bulletSoundPlayer.clip = audioClip;
-                        }
+                            if (newSprite != null)
+                            {
+                                bulletObject.GetComponent<SpriteRenderer>().sprite = newSprite;
+                            }
+                            if (audioClip != null)
+                            {
+                                bulletSoundPlayer.clip = audioClip;
+                            }
 
+                        }
                     }
+
                 }
 
             }
